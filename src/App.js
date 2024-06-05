83d2e4fd9be2946f5b812d7ddfc0adb2c5fc80a9
Param Savjani <paramsavjani3010@gmail.com>
2024-06-05 05:38:12 +0530

    f

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     }
   }, [dispatch]);
 
-  fetch("https://todo-backend-param.onrender.com").then((res) => res.text()).then((data) => console.log(data)).catch((err) => console.log(err));
+  // fetch("https://todo-backend-param.onrender.com").then((res) => res.text()).then((data) => console.log(data)).catch((err) => console.log(err));
 
   return (
     <Router>
